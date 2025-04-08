2437a5391ffe1aff4449a2fc5d55d970d872fd45
Mahabub-Hasan-Bayzid <s2500143@edu.bc.fi>
2025-04-08 13:13:19 +0300

    HR APP First Step

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      &copy; 2025 REACT25K
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = ({ appName }) => {
+  return (
+    <header className="header">
+      <h1>{appName}</h1>
+    </header>
+  );
+};
+
+export default Header;
